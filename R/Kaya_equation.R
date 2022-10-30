11a3f5c799737176fd3674ea5e9b7edd3c9124f1
Thuy Ngoc Phuong Ta <50316526+thuyngocphuongta@users.noreply.github.com>
2022-10-30 14:38:42 +0100

    changed the Kaya equation function v2

--- R/Kaya_equation.R
+++ R/Kaya_equation.R
@@ -1,40 +1,50 @@
-# This is an example function named 'Kaya Equation'
-# which expresses yearly CO2 emissions
-
 # First, import all necessary packages
 library(devtools)
 library(checkmate)
 
 
-#' Calculation of co2 Emissions yearly
+#' Calculation of either the tonnes of Carbon instead of CO2 Emissions yearly
+#' or the CO2 emissions
 #'
 #' @param pop numeric indicating Population size (in millions)
 #' @param gdp numeric indicating GDP per capita (in 1000$/person)
 #' @param enInt numeric indicating Energy intensity (in Gigajoule/$1000GDP)
 #' @param carbInt numeric indicating Carbon intensity (in tonnes CO2/Gigajoule)
+#' @param output_type character either "CO2" or "C"
 #'
 #' @return numeric representing yearly co2 emissions of a country
 #' @author Thuy Ngoc Phuong Ta
 #' @export
 #'
 #' @examples
-#' Kaya_equation(82.4,44,5,0.05)
-Kaya_equation <- function(pop, gdp, enInt, carbInt) {
+#' Kaya_equation(82.4, 44, 5, 0.05, "CO2")
+Kaya_equation<- function(pop, gdp, enInt, carbInt, output_type = "CO2") {
   assertNumeric(pop, any.missing = FALSE, lower = 0)
   assertNumeric(gdp, any.missing = FALSE, lower = 0)
   assertNumeric(enInt, any.missing = FALSE, lower = 0)
   assertNumeric(carbInt, any.missing = FALSE, lower = 0)
 
   co2 = pop * gdp * enInt * carbInt
-  return(co2)
+  if(output_type == "CO2"){
+    return(co2)
+  }
+  else
+  {
+    return(co2/3.67)
+  }
+
 }
 
 # Inputs of Germany:
-  # pop = 82.4
-  # gdp = 44
-  # enInt = 5
-  # carbInt = 0.05
+# pop = 82.4
+# gdp = 44
+# enInt = 5
+# carbInt = 0.05
 
 # Calculating co2 emissions of Germany
-Kaya_equation(82.4,44,5,0.05)
+Kaya_equation(82.4,44,5,0.05,"CO2")
 # 906.4
+
+# Calculating the tonnes of Carbon (C)
+Kaya_equation(82.4,44,5,0.05, "C")
+# 246.9755
